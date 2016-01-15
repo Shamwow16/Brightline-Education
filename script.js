ce49a0020bdd2f0485b21df51cfc7a7e51a6c675
Shamwow16 <shamyleg@gmail.com>
2016-01-14 23:00:46 -0600

    Commit 1

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("nav-menu").innerHTML =  '<div class="container">' +
+    '<div class="navbar-header">' +
+      '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">' +
+        '<span class="sr-only">Toggle navigation</span>' +
+        '<span class="icon-bar"></span>' +
+        '<span class="icon-bar"></span>' +
+        '<span class="icon-bar"></span>' +
+      '</button>' +
+      '<a href="index.html">' + '<img src="img/brightline_logo.png">' + '</a>' +
+    '</div>' +
+
+    '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">' +
+      '<ul class="nav navbar-nav">' +
+        '<li><a href="about-us.html">About Us</a></li>' +
+        '<li><a href="#">How It Works</a></li>' +
+        '<li><a href="#">Get Involved</a></li>' +
+        '<li><a href="contact-us.html">Contact Us</a></li>' +
+      '</ul>' +
+    '</div>'+
+  '</div>';
+
+
