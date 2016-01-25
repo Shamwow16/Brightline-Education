0c734ddfb0bbfcb0e410ed263377346af20672e0
Shamwow16 <shamyleg@gmail.com>
2016-01-25 17:32:59 -0600

    Release 1 with responsiveness

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
-/*document.getElementById("nav-menu").innerHTML =  '<div class="navbar-header">' +
-            '<button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">' +
-                '<span class="sr-only">Toggle navigation</span>' +
-                '<span class="icon-bar"></span>' +
-                '<span class="icon-bar"></span>' +
-                '<span class="icon-bar"></span>' +
-            '</button>' +
-            '<a href="index.html" class="navbar-brand"><img src="img/brightline_logo.png"></a>' +
-        '</div>' +
+document.getElementById("nav-menu").innerHTML =  '<div class="navbar-header">' +
+              '<a href="index.html" class="navbar-brand text-center"><img class="img-responsive logo" id="brightline-logo-large-screen" src="img/brightline.png"><img id="brightline-logo-small-screen" class="img-responsive logo" src="img/bulb.png" style="display:none;"></a>' +
+'<button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">' +
+      '<span class="sr-only">Toggle navigation</span>' +
+      '<span class="icon-bar"></span>' +
+      '<span class="icon-bar"></span>' +
+      '<span class="icon-bar"></span>' +
+      '</button>' +
+      '</div>' +
 
-        '<div id="navbarCollapse" class="collapse navbar-collapse">' +
-        '<ul class="nav navbar-nav">' +
+      '<div id="navbarCollapse" class="collapse navbar-collapse">' +
+      '<ul class="nav navbar-nav">' +
+      '<li class="dropdown">' + '<a data-toggle="dropdown" class="dropdown-toggle" href="#">About Us</a>' +
+      '<ul role="menu" class="dropdown-menu">' +
+      '<li><a href="our-mission.html">Our Mission</a></li>' +
+      '<li><a href="our-goals.html">Our Goals</a></li>' +
+      '<li><a href="how-it-works.html">How It Works</a></li>' + '</li>' +
+      '<li><a href="stem-education.html">Importance of STEM Education</a></li>' + '</ul>' + '</li>' +
+      '<li><a href="our-team.html">Our Team</a></li>' +
+      '<li class="dropdown">' + '<a data-toggle="dropdown" class="dropdown-toggle" href="#">Get Involved</a>' + '<ul role="menu" class="dropdown-menu">' +
+      '<li><a href="volunteer-survey.html">Volunteer Survey</a></li>' +
+      '<li><a href="sponsor.html">Sponsor A Child</a></li>' +
+      '<li><a href="donate.html">Donate</a></li>' + '</ul>' + '</li>' +
+      '<li><a href="contact-us.html">Contact Us</a></li>' +
+      '</ul>' +
+      '</div>'
 
-                '<li class="dropdown">' +
-                    '<a data-toggle="dropdown" class="dropdown-toggle" href="#">About Us</a>' +
-                    '<ul role="menu" class="dropdown-menu">' +
-                        '<li><a href="our-mission.html">Our Mission</a></li>' +
-                        '<li><a href="#">How It Works</a></li>' +
-                    '</ul>' +
-                '</li>' +
-
-                '<li><a href="our-team.html">Our Team</a></li>' +
-
-                 '<li class="dropdown">' +
-                    '<a data-toggle="dropdown" class="dropdown-toggle" href="#">Get Involved</a>' +
-                    '<ul role="menu" class="dropdown-menu">' +
-                        '<li><a href="volunteer-survey.html">Volunteer Survey</a></li>' +
-                        '<li><a href="#">Sponsor A Child</a></li>' +
-                        '<li><a href="#">Donate</a></li>' +
-                    '</ul>' +
-                '</li>' +
-                '<li><a href="contact-us.html">Contact Us</a></li>' +
-            '</ul>' +
-            /*'<form role="search" class="navbar-form navbar-right">' +
-                '<div class="form-group">' +
-                    '<input type="text" placeholder="Search" class="form-control">' +
-                '</div>' +
-            '</form>' +*/
-        /*'</div>'*/
 
 document.getElementById('footer-section').innerHTML = '<div class="container">' +
   '<div class="row">' +
@@ -62,6 +50,20 @@ document.getElementById('footer-section').innerHTML = '<div class="container">' 
   '</div>' +
   '</div>' +
   '</div>'
+
+ var mainPageQuote = $("#main-page-quote");
+/*  if($(window).width() < 600) {
+    mainPageQuote.hide();
+  }
+*/
+ /* var mq = window.matchMedia("(max-width:600px)");
+  if(mq.matches){
+    mainPageQuote.hide();
+  }
+
+  else{
+    mainPageQuote.show();
+  }*/
 
 
 
